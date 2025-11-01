724696c032ef0f02394c018fee249380ac97764e
KaiTheRedNinja <88234730+KaiTheRedNinja@users.noreply.github.com>
2025-11-01 15:37:11 +0800

    create title bar

--- src/sections/titleBar.tsx
+++ src/sections/titleBar.tsx
@@ -2,7 +2,62 @@
 
 import { Flag, Menu } from "lucide-react";
 
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
 export default function TitleBar() {
+  const sectionNavigator = (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline"><Menu className="w-6 h-6 text-gray-700" /></Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56" align="start">
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            Home
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            About Us
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            Programmes and Courses
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            Testimonials
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            Blog
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            Contact Us
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            FAQ
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            Register
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+
   return (
     <div className="flex items-center justify-between px-4 py-3">
       {/* Left side: Circle + Title */}
@@ -16,9 +71,7 @@ export default function TitleBar() {
         <button className="p-2 hover:bg-gray-100 rounded-lg transition-colors">
           <Flag className="w-6 h-6 text-gray-700" />
         </button>
-        <button className="p-2 hover:bg-gray-100 rounded-lg transition-colors">
-          <Menu className="w-6 h-6 text-gray-700" />
-        </button>
+        {sectionNavigator}
       </div>
     </div>
   );
